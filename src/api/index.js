efae10bf5331bb17f2e75df292026fbcf8e2e565
13circle <13circle97@gmail.com>
2020-12-19 13:48:16 +0900

    Add /posts and /comments APIs

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,14 @@
 import Router from "koa-router";
 
 import user from "./user";
+import post from "./post";
+import comment from "./comment";
 
 const api = new Router();
 
 api.use("/users", user.routes());
+api.use("/posts", post.routes());
+api.use("/comments", comment.routes());
 
 export default api;
 
