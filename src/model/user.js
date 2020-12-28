dbb458c5d9be698d304bea968b69a6f1bfcf185d
13circle <13circle97@gmail.com>
2020-12-28 21:53:51 +0900

    Add jwt token generator method

--- src/model/user.js
+++ src/model/user.js
@@ -1,57 +1,65 @@
 import { model, Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const BCRYPT_SALT_ROUNDS = 10;
 
-const UserSchema = new Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-  nickname: { type: String, unique: true, required: true },
-  privilege: {
-    type: String,
-    enum: [ "SysAdmin", "Admin", "User" ],
-    required: true,
+const UserSchema = new Schema(
+  {
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    nickname: { type: String, unique: true, required: true },
+    privilege: {
+      type: String,
+      enum: ["SysAdmin", "Admin", "User"],
+      default: "User",
+      required: true,
+    },
+    myPosts: {
+      type: [Schema.Types.ObjectId],
+      ref: "Post",
+      required: true,
+    },
+    likedPosts: {
+      type: [Schema.Types.ObjectId],
+      ref: "Post",
+      required: true,
+    },
+    favoredTags: {
+      type: [Schema.Types.ObjectId],
+      ref: "Tag",
+      required: true,
+    },
   },
-  myPosts: {
-    type: [ Schema.Types.ObjectId ],
-    ref: "Post",
-    required: true,
+  {
+    timestamps: {
+      createdAt: "createAt",
+      updatedAt: "updatedAt",
+    },
   },
-  likedPosts: {
-    type: [ Schema.Types.ObjectId ],
-    ref: "Post",
-    required: true,
-  },
-  favoredTags: {
-    type: [ Schema.Types.ObjectId ],
-    ref: "Tag",
-    required: true,
-  },
-}, {
-  timestamps: {
-    createdAt: "createAt",
-    updatedAt: "updatedAt",
-  },
-});
+);
 
-UserSchema.methods.serialize = function() {
+UserSchema.methods.serialize = function () {
   const data = this.toJSON();
   delete data.password;
   return data;
 };
 
-UserSchema.methods.setPassword = async function(password) {
+UserSchema.methods.setPassword = async function (password) {
   const salt = await bcrypt.genSalt(BCRYPT_SALT_ROUNDS);
   const hash = await bcrypt.hash(password, salt);
   this.password = hash;
 };
 
-UserSchema.methods.checkPassword = async function(password) {
+UserSchema.methods.checkPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.getToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
 const User = model("User", UserSchema);
 
 export default User;
-
