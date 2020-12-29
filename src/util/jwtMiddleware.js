53df2916afbb5cb3774cbdf5897bcfea6ab0293b
13circle <13circle97@gmail.com>
2020-12-29 21:18:12 +0900

    Handle JWT with middelware

---
+++ src/util/jwtMiddleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export const jwtMiddleware = (ctx, next) => {
+  const token = ctx.cookies.get("access_token");
+  if (!token) return next();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    ctx.state.user = {
+      _id: decoded._id,
+      username: decoded.nickname,
+    };
+    console.log(decoded);
+    return next();
+  } catch (e) {
+    return next();
+  }
+};
