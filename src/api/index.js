52e9aef6bec682b5af2916d130bfd1cd6d9a428e
13circle <13circle97@gmail.com>
2021-01-11 03:21:09 +0900

    Add /api/tags as API

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,13 @@ import Router from "koa-router";
 import user from "./user";
 import post from "./post";
 import comment from "./comment";
+import tag from "./tag";
 
 const api = new Router();
 
 api.use("/users", user.routes());
 api.use("/posts", post.routes());
 api.use("/comments", comment.routes());
+api.use("/tags", tag.routes());
 
 export default api;
-
