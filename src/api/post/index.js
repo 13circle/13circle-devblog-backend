2b5642fdfa665d826be160f4cd826a2235e0ff6e
13circle <13circle97@gmail.com>
2021-01-12 00:38:20 +0900

    Complete /api/posts APIs except /api/posts/search
    * GET /api/posts
      - List posts with pagination
      - Shorten content length
    * GET /api/posts/user/:author
      - Change /api/posts/mylist to /api/posts/user
      - List posts by :author
      - Shornten content length
    * GET /api/posts/:id
      - Get post by :id
      - Content not shortened
    * POST /api/posts
      - Add new post
      - Also add new post id to User.myPosts
      - Can add original tags and new tags
    * PATCH /api/posts/:id
      - Update post by :id
      - Can modify tags and add new tags
    * DELETE /api/posts/:id
      - Delete post by :id
      - Also delete the post in User.myPosts

--- src/api/post/index.js
+++ src/api/post/index.js
@@ -1,15 +1,20 @@
 import Router from "koa-router";
 
+import checkLoginStatus from "../../util/checkLoginStatus";
+import getCurrentPost from "../../util/getCurrentPost";
+import checkPostOwner from "../../util/checkPostOnwer";
+import mapTagNameToId from "../../util/mapTagNameToId";
+
 import * as postsCtrl from "./post.ctrl";
 
 const posts = new Router();
 
 posts.get("/", postsCtrl.list);
-posts.get("/mylist/:author", postsCtrl.mylist);
-posts.get("/doc/:id", postsCtrl.doc);
+posts.get("/user/:author", postsCtrl.userPosts);
+posts.get("/:id", getCurrentPost, postsCtrl.doc);
 posts.get("/search", postsCtrl.search);
-posts.post("/", postsCtrl.add);
-posts.patch("/:id", postsCtrl.edit);
-posts.delete("/:id", postsCtrl.remove);
+posts.post("/", checkLoginStatus, mapTagNameToId, postsCtrl.add);
+posts.patch("/:id", checkLoginStatus, getCurrentPost, checkPostOwner, mapTagNameToId, postsCtrl.edit);
+posts.delete("/:id", checkLoginStatus, getCurrentPost, checkPostOwner, postsCtrl.remove);
 
 export default posts;
--- src/api/post/post.ctrl.js
+++ src/api/post/post.ctrl.js
@@ -1,46 +1,76 @@
-import Joi from "joi"
+import Joi from "joi";
 
 import User from "../../model/user";
 import Post from "../../model/post";
 
+const numPostsPerPage = 10;
+const maxContentLen = 150;
+
 export const list = async (ctx) => {
-  ctx.body = "GET /posts";
-};
+  const page = parseInt(ctx.query.page || "1", 10);
 
-export const mylist = async (ctx) => {
-  const schema = Joi.object().keys({
-    author: Joi.string().required(),
-  });
-
-  const result = schema.validate(ctx.request.params);
-  if (result.error) {
+  if (page < 1) {
     ctx.status = 400;
-    ctx.body = result.error;
     return;
   }
 
-  const { author } = ctx.request.params;
-
   try {
-    const post = await User.findOne({ nickname: author }).populate("myPosts");
-    if (post.length === 0) {
-      ctx.status = 404;
-      return;
-    }
+    const posts = await Post.find({})
+      .populate("author", "nickname")
+      .populate("tags", "tagName")
+      .sort({ _id: -1 })
+      .limit(numPostsPerPage)
+      .skip((page - 1) * numPostsPerPage)
+      .exec();
+    const postCount = await Post.countDocuments().exec();
 
-    ctx.body = post.map((d) => d.toJSON);
+    ctx.body = {
+      lastPage: Math.ceil(postCount / numPostsPerPage),
+      posts: posts.map((post) => ({
+        ...post.toJSON(),
+        content: post.getBriefContent(maxContentLen),
+      })),
+    };
   } catch (e) {
     ctx.throw(500, e);
   }
+};
 
-  ctx.body = "GET /posts/mylist/" + ctx.request.params.author;
+export const userPosts = async (ctx) => {
+  const { author } = ctx.request.params;
+
+  if (!author) {
+    ctx.status = 400;
+    return;
+  }
+
+  try {
+    const user = await User.findOne({ nickname: author });
+    if (!user) {
+      ctx.status = 400;
+      return;
+    }
+
+    const posts = user.toJSON().myPosts;
+    for (let i in posts) {
+      const post = await Post.findById(posts[i]).populate("tags").exec();
+      post.content = post.getBriefContent(maxContentLen);
+      posts[i] = post.toJSON();
+    }
+
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const doc = async (ctx) => {
-  const schema = Joi.object().keys({
-    id: Joi.string().required(),
-  });
-  ctx.body = "GET /posts/doc/" + ctx.request.params.id;
+  const { post } = ctx.state;
+  try {
+    ctx.body = await Post.findById(post._id).populate("tags", "tagName");
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const search = async (ctx) => {
@@ -48,27 +78,82 @@ export const search = async (ctx) => {
 };
 
 export const add = async (ctx) => {
-  ctx.body = "POST /posts";
-};
-
-export const edit = async (ctx) => {
-  const schema = Joi.object().keys({
-    id: Joi.string().required(),
+  const schema = Joi.object({
+    title: Joi.string().min(3).required(),
+    content: Joi.string().required(),
+    tags: Joi.array().required(),
   });
 
-  const result = schema.validate(ctx.request.params);
+  const result = schema.validate(ctx.request.body);
   if (result.error) {
     ctx.status = 400;
     ctx.body = result.error;
     return;
   }
 
-  ctx.body = "PATCH /posts/" + ctx.request.params.id;
+  const { title, content } = ctx.request.body;
+  const { user, tags } = ctx.state;
+
+  try {
+    const isTitleExsits = await Post.findOne({ title });
+    if (isTitleExsits) {
+      ctx.status = 409;
+      return;
+    }
+
+    const post = new Post({
+      author: user.id,
+      tags,
+      title,
+      content,
+    });
+    await post.save();
+
+    const userModel = await User.findById(user.id);
+    userModel.myPosts.push(post._id);
+    await userModel.save();
+
+    ctx.body = await Post.findById(post._id).populate("tags");
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const edit = async (ctx) => {
+  const schema = Joi.object().keys({
+    title: Joi.string().min(3),
+    content: Joi.string(),
+    tags: Joi.array(),
+  });
+
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { id } = ctx.params;
+
+  try {
+    const modified = await Post.findByIdAndUpdate(id, { ...ctx.request.body }, { new: true });
+
+    ctx.body = modified;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const remove = async (ctx) => {
-  const schema = Joi.object().keys({
-    id: Joi.string().required(),
-  });
-  ctx.body = "DELETE /posts/" + ctx.request.params.id;
+  const { id } = ctx.params;
+  const { user } = ctx.state;
+
+  try {
+    await User.findByIdAndUpdate(user.id, { $pull: { myPosts: id } }, {});
+    await Post.findByIdAndDelete(id);
+
+    ctx.status = 204;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
