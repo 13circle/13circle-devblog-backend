461e1d49e1c5a39dde4bde1d6b55c53a9bb2de99
13circle <13circle97@gmail.com>
2021-01-11 18:14:43 +0900

    Map tags to Tag ids & add new Tags

---
+++ src/util/mapTagNameToId.js
@@ -0,0 +1,46 @@
+import Joi from "joi";
+
+import Tag from "../model/tag";
+
+const mapTagNameToId = async (ctx, next) => {
+  const result = Joi.object({
+    tags: Joi.array().min(1).required(),
+  })
+    .unknown()
+    .validate(ctx.request.body);
+
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { tags } = ctx.request.body;
+
+  if (!tags) {
+    ctx.status = 400;
+    return;
+  }
+
+  try {
+    for(let i in tags) {
+      const tag = await Tag.findOne({ tagName: tags[i] });
+
+      if(tag) {
+        tags[i] = tag._id;
+      } else {
+        const newTag = new Tag({ tagName: tags[i] });
+        await newTag.save();
+        tags[i] = newTag._id;
+      }
+    }
+
+    ctx.state.tags = tags;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+
+  return next();
+};
+
+export default mapTagNameToId;
