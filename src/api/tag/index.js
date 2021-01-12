7969dc64c34ed5fbc14421ac0797bfa98ee41380
13circle <13circle97@gmail.com>
2021-01-13 00:09:32 +0900

    Add checkIdParam as a middleware

--- src/api/tag/index.js
+++ src/api/tag/index.js
@@ -3,11 +3,12 @@ import Router from "koa-router";
 import * as tagsCtrl from "./tags.ctrl";
 import checkLoginStatus from "../../util/checkLoginStatus";
 import checkAdmin from "../../util/checkAdmin";
+import checkIdParam from "../../util/checkIdParam";
 
 const tags = new Router();
 
 tags.get("/", tagsCtrl.list);
 tags.post("/:tagName", checkLoginStatus, tagsCtrl.create);
-tags.delete("/:id", checkLoginStatus, checkAdmin, tagsCtrl.remove);
+tags.delete("/:id", checkIdParam, checkLoginStatus, checkAdmin, tagsCtrl.remove);
 
 export default tags;
