1b9ddada6f5d75e68fde5f9fb2f0d2a7242c74e1
13circle <13circle97@gmail.com>
2021-01-12 23:27:29 +0900

    Apply checkEmailConfirmed middleware

--- src/api/post/index.js
+++ src/api/post/index.js
@@ -2,6 +2,7 @@ import Router from "koa-router";
 
 import checkLoginStatus from "../../util/checkLoginStatus";
 import checkIdParam from "../../util/checkIdParam";
+import checkEmailConfirmed from "../../util/checkEmailConfirmed";
 import getCurrentPost from "../../util/getCurrentPost";
 import checkPostOwner from "../../util/checkPostOnwer";
 import mapTagNameToId from "../../util/mapTagNameToId";
@@ -13,7 +14,7 @@ const posts = new Router();
 posts.get("/", postsCtrl.list);
 posts.get("/user/:author", postsCtrl.userPosts);
 posts.get("/:id", checkIdParam, getCurrentPost, postsCtrl.doc);
-posts.post("/", checkLoginStatus, mapTagNameToId, postsCtrl.add);
+posts.post("/", checkLoginStatus, checkEmailConfirmed, mapTagNameToId, postsCtrl.add);
 posts.patch("/:id", checkLoginStatus, checkIdParam, getCurrentPost, checkPostOwner, mapTagNameToId, postsCtrl.edit);
 posts.delete("/:id", checkLoginStatus, checkIdParam, getCurrentPost, checkPostOwner, postsCtrl.remove);
 
