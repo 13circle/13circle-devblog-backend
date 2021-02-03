d1d1daf90fa090dade8289f8632db42167fcd30c
13circle <13circle97@gmail.com>
2021-02-03 23:41:43 +0900

    Controller for /api/comments/list/:id API
    With pagination applied

--- src/api/comment/comment.ctrl.js
+++ src/api/comment/comment.ctrl.js
@@ -3,8 +3,27 @@ import Joi from "joi";
 import User from "../../model/user";
 import Comment from "../../model/comment";
 
+const numCommentsPerPage = 30;
+
 export const listByPost = async (ctx) => {
-  ctx.body = "GET /comments/:id";
+  const page = parseInt(ctx.query.page || "1", 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
+  try {
+    const comments = await Comment.find({ post: ctx.state.post._id })
+      .sort({ _id: -1 })
+      .limit(numCommentsPerPage)
+      .skip((page - 1) & numCommentsPerPage)
+      .exec();
+
+    ctx.body = comments;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const add = async (ctx) => {
