501321791e6e23fa8dbc6cbdc63c12b1e6bc6323
13circle <13circle97@gmail.com>
2021-02-04 23:35:31 +0900

    Controller for /api/comments/add/:id API

--- src/api/comment/comment.ctrl.js
+++ src/api/comment/comment.ctrl.js
@@ -1,7 +1,9 @@
 import Joi from "joi";
+import mongoose from "mongoose";
 
 import User from "../../model/user";
 import Comment from "../../model/comment";
+import Post from "../../model/post";
 
 const numCommentsPerPage = 30;
 
@@ -27,7 +29,60 @@ export const listByPost = async (ctx) => {
 };
 
 export const add = async (ctx) => {
-  ctx.body = "POST /comments";
+  const schema = Joi.object().keys({
+    content: Joi.string().required(),
+    parent: Joi.string(),
+  });
+
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { content } = ctx.request.body;
+  const parent = ctx.request.body.parent || null;
+
+  if (parent && !mongoose.Types.ObjectId.isValid(parent)) {
+    ctx.status = 400;
+    return;
+  }
+
+  try {
+    const parentComment = await Comment.findById(parent);
+
+    if (!parentComment) {
+      ctx.status = 404;
+      return;
+    }
+
+    const { user, post } = ctx.state;
+
+    const comment = new Comment({
+      author: user.id,
+      post: post._id,
+      content,
+      parent,
+    });
+
+    await comment.save();
+
+    await Post.findByIdAndUpdate(
+      post._id,
+      {
+        "$push": { "comments": comment._id },
+      },
+      {
+        "new": true,
+        "upsert": true,
+      }
+    );
+
+    ctx.body = comment;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const edit = async (ctx) => {
