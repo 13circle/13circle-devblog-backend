daf62048c437072ee0bccdf5ec1eab48edb72b48
13circle <13circle97@gmail.com>
2021-02-17 22:14:42 +0900

    Stop paginating Comments

--- src/api/comment/comment.ctrl.js
+++ src/api/comment/comment.ctrl.js
@@ -4,22 +4,9 @@ import mongoose from "mongoose";
 import Comment from "../../model/comment";
 import Post from "../../model/post";
 
-const numCommentsPerPage = 30;
-
 export const listByPost = async (ctx) => {
-  const page = parseInt(ctx.query.page || "1", 10);
-
-  if (page < 1) {
-    ctx.status = 400;
-    return;
-  }
-
   try {
-    const comments = await Comment.find({ post: ctx.state.post._id })
-      .sort({ _id: -1 })
-      .limit(numCommentsPerPage)
-      .skip((page - 1) & numCommentsPerPage)
-      .exec();
+    const comments = await Comment.find({ post: ctx.state.post._id }).sort({ _id: -1 }).exec();
 
     ctx.body = comments;
   } catch (e) {
@@ -70,12 +57,12 @@ export const add = async (ctx) => {
     await Post.findByIdAndUpdate(
       post._id,
       {
-        "$push": { "comments": comment._id },
+        $push: { comments: comment._id },
       },
       {
-        "new": true,
-        "upsert": true,
-      }
+        new: true,
+        upsert: true,
+      },
     );
 
     ctx.body = comment;
